6d2f5a48b27c9ffa2c1d8003cf0a67e6a67eda38
Qingyang-Sophia <caoqingyang2002@gmail.com>
2022-09-22 05:13:47 -0400

    Try different if

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -7,7 +7,8 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.toLowerCase().contains("pineapple")) {
+        } 
+        if (query.toLowerCase().contains("pineapple")) {
             return "Pineapple is a tropical plant with an edible fruit.";
         }
         return "";
